7c9ce87013c1ec66d7631aa403f4f5ecbfd15e56
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2019-11-01 11:27:32 +0100

    Change in pipeline

--- tasks/index.js
+++ tasks/index.js
@@ -1,16 +1,22 @@
 const gulp = require('gulp')
 
+const isDev = process.env.NODE_ENV !== 'production'
+
 const { scripts } = require('./webpack')
-const { server } = require('./server')
 const { modernizr } = require('./modernizr')
 const { combineHtml, processHtml, watchHtml } = require('./html')
 
-const watch = gulp.parallel(watchHtml, server)
+if (isDev) {
+  const { server } = require('./server')
+  const watch = gulp.parallel(watchHtml, server)
+  const dev = gulp.series(modernizr, html, watch)
+  exports.dev = dev
+}
+
+
 const html = gulp.series(combineHtml, processHtml)
-const dev = gulp.series(modernizr, html, watch)
 const build = gulp.series(modernizr, scripts)
 
-exports.dev = dev
 exports.build = build
 exports.modernizr = modernizr
 exports.html = html
