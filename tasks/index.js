dbdf0733d6e97067eff23f3a6c68f2191dd38368
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2019-11-01 11:31:04 +0100

    another one

--- tasks/index.js
+++ tasks/index.js
@@ -4,20 +4,22 @@ const isDev = process.env.NODE_ENV !== 'production'
 
 const { scripts } = require('./webpack')
 const { modernizr } = require('./modernizr')
-const { combineHtml, processHtml, watchHtml } = require('./html')
+
 
 if (isDev) {
   const { server } = require('./server')
+  const { combineHtml, processHtml, watchHtml } = require('./html')
+
+  const html = gulp.series(combineHtml, processHtml)
   const watch = gulp.parallel(watchHtml, server)
   const dev = gulp.series(modernizr, html, watch)
   exports.dev = dev
+  exports.html = html
+  exports.default = dev
 }
 
-
-const html = gulp.series(combineHtml, processHtml)
 const build = gulp.series(modernizr, scripts)
 
 exports.build = build
 exports.modernizr = modernizr
-exports.html = html
-exports.default = dev
+
